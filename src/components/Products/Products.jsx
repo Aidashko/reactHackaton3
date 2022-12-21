24f275bf0557f300c008942e2ce181e360609354
Aidashko <aidana20051318@gmail.com>
2022-12-21 21:21:08 +0600

    .

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div class="products-container">
+      <div class="box">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0554/6301/8688/products/bobby-c-marron1_96bf2ad3-c763-4bb9-ac55-4036fae28cef.jpg?v=1661180669"
+          alt=""
+        />
+        <h3>Boris</h3>
+        <div class="content">
+          <span>$199.00</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+      <div class="box">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0554/6301/8688/products/pablo_cuir_noir1-min_a5deaa0a-616e-4d54-a414-5afb42567b54.jpg?v=1660729491"
+          alt=""
+        />
+        <h3>Pablo</h3>
+        <div class="content">
+          <span>$243.00</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+      <div class="box">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0554/6301/8688/products/roller-cuir-marine-1.jpg?v=1634911199"
+          alt=""
+        />
+        <h3>Roller</h3>
+        <div class="content">
+          <span>$98</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+      <div class="box">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0554/6301/8688/products/RUDYSUMMER-24.jpg?v=1651499192"
+          alt=""
+        />
+        <h3>Mayork</h3>
+        <div class="content">
+          <span>$98</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+      <div class="box">
+        <img
+          src="https://img2.storyblok.com/1248x1540/filters:quality(90)/f/124941/1260x1540/c8f1b6d638/demi-push-boots.jpg"
+          alt=""
+        />
+        <h3>Сooper</h3>
+        <div class="content">
+          <span>$98</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+      <div class="box">
+        <img
+          src="https://www.markajj.com/data/urun/7OIN1kCnmDcpaZRb.jpg"
+          alt=""
+        />
+        <h3>Turkiye nast</h3>
+        <div class="content">
+          <span>$98</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+      <div class="box">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0554/6301/8688/products/RUDYSUMMER-12.jpg?v=1651502252"
+          alt=""
+        />
+        <h3>Lacoste Lobb</h3>
+        <div class="content">
+          <span>$98</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+      <div class="box">
+        <img
+          src="https://www.markajj.com/v40/image/data/urun/31MoS3OaFYChFqB1.jpg"
+          alt=""
+        />
+        <h3>BEIGE CHELSEA</h3>
+        <div class="content">
+          <span>$98</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+      <div class="box">
+        <img
+          src="https://media.shopimind.com/clients/shopFiles/3800/6329e07e74b86.jpg"
+          alt=""
+        />
+        <h3>Chelsea Boots</h3>
+        <div class="content">
+          <span>$98</span>
+          <a href="#" class="btn-cart">
+            Add to cart
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
